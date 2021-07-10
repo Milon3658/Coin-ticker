8308c8afc5cebf780dc75db721b5372fbe86fdc2
shihabflutter <shihab@aapbd.com>
2021-07-11 03:48:47 +0600

    * Front page UI done

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
@@ -6,10 +8,51 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  DropdownButton<String> AndroidPicker() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(
+          currency,
+        ),
+        value: currency,
+      );
+      dropDownItems.add(newItem);
+    }
+
+    return DropdownButton<String>(
+        value: SelectedCurrency,
+        items: dropDownItems,
+        onChanged: (value) {
+          setState(() {
+            SelectedCurrency = value;
+          });
+        });
+  }
+
+  CupertinoPicker IOSpicker() {
+    List<Text> pickerItem = [];
+
+    for (String currency in currenciesList) {
+      pickerItem.add(Text(currency));
+    }
+    return CupertinoPicker(
+      backgroundColor: Colors.pink,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: pickerItem,
+    );
+  }
+
+  String SelectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('🤑 Coin Ticker'),
       ),
       body: Column(
@@ -19,7 +62,7 @@ class _PriceScreenState extends State<PriceScreen> {
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
-              color: Colors.lightBlueAccent,
+              color: Colors.pink,
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
@@ -38,11 +81,11 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
+            height: 130.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
+            color: Colors.pink,
+            child: AndroidPicker(),
           ),
         ],
       ),
